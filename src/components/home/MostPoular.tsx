0bad92bf0d0281ba6a7aa8af34136cf68ddede79
Rajesh6290 <rajeshkumarbehera039@gmail.com>
2024-04-07 18:40:34 +0530

    skelton added

--- src/components/home/MostPoular.tsx
+++ src/components/home/MostPoular.tsx
@@ -19,9 +19,25 @@ const MostPopular = () => {
           <p className="h-1 w-48 bg-primary rounded-full"></p>
         </article>
         <div className=" w-full h-full grid 2xl:grid-cols-5 lg:grid-cols-4 md:grid-cols-3 grid-cols-2  gap-5 ">
-          {item.slice(0, 10).map((curEle: any) => (
-            <ProductCard item={curEle} key={curEle.id} />
-          ))}
+
+          {
+            isValidating ? (
+              <>
+                <Skelton />
+                <Skelton />
+                <Skelton />
+                <Skelton />
+                <Skelton />
+                <Skelton />
+                <Skelton />
+                <Skelton />
+                <Skelton />
+                <Skelton />
+              </>
+            ) : (
+              item.slice(0, 10).map((curEle: any) => (
+                <ProductCard item={curEle} key={curEle.id} />
+              )))}
         </div>
         <article className="flex items-center justify-center">
           <Link
@@ -40,3 +56,66 @@ const MostPopular = () => {
 };
 
 export default MostPopular;
+
+export const Skelton = () => {
+  return (
+    <div
+      className="relative bg-slate-100 h-full group overflow-hidden  w-full flex flex-col gap-2 justify-between items-center  shadow-[0px_0px_4px_0px_#00000024] rounded-lg p-4"
+
+    >
+
+
+      <div className=" w-full flex  flex-col gap-4">
+        <Link href={``}>
+          <div
+
+            className=" w-full object-contain bg-slate-200 animate-pulse md:h-36 h-24 object-fil rounded-lg cursor-pointer group-hover:scale-105 duration-300 "
+
+          >
+          </div>
+        </Link>
+        <div className=" flex w-full flex-col gap-2">
+          <div className=" flex md:flex-row flex-col md:items-center items-start justify-between">
+            <p className=" flex items-center  gap-1">
+              <span className=" w-32 p-2 rounded bg-slate-200 animate-pulse">
+
+              </span>
+              <span className="w-16 p-1 rounded bg-gray-200 animate-pulse">
+
+              </span>
+            </p>
+            <p className="w-10 p-2 rounded bg-slate-200 animate-pulse">
+
+            </p>
+
+
+
+          </div>
+          <p className="w-40 p-2 bg-slate-200 animate-pulse">
+
+          </p>
+          <p className="text-sm flex items-center gap-1">
+            <p className="w-5 h-5 rounded bg-slate-200 animate-pulse"></p>
+            <p className="w-5 h-5 rounded bg-slate-200 animate-pulse"></p>
+            <p className="w-5 h-5 rounded bg-slate-200 animate-pulse"></p>
+            <p className="w-5 h-5 rounded bg-slate-200 animate-pulse"></p>
+            <p className="w-5 h-5 rounded bg-slate-200 animate-pulse"></p>
+          </p>
+          <div className=" flex justify-between items-center  ">
+            <p className=" flex flex-col sm:flex-row items-center gap-1">
+              <span className="w-14 p-2 rounded bg-slate-200 animate-pulse"></span>
+              <span className="w-14 p-2 rounded bg-slate-200 animate-pulse">
+
+              </span>
+            </p>
+            <Link href="/cart">
+              <p className=" w-10 h-10 rounded-md bg-slate-200 animate-pulse">
+
+              </p>
+            </Link>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
--- src/components/products/AllProducts.tsx
+++ src/components/products/AllProducts.tsx
@@ -1,72 +1,25 @@
 import useSwr from "@/hooks/useSwr";
 import ProductCard from "../home/ProductCard";
+import { Skelton } from "../home/MostPoular";
 
 const AllProducts = () => {
-  const PRODUCT_ARR = [
-    {
-      id: "1",
-      image: "/product/p1.webp",
-      name: "Digital BP Machine",
-    },
-    {
-      id: "1",
-      image: "/product/p2.webp",
-      name: "Digital BP Machine",
-    },
-    {
-      id: "1",
-      image: "/product/p3.webp",
-      name: "Digital BP Machine",
-    },
-    {
-      id: "1",
-      image: "/product/p4.webp",
-      name: "Digital BP Machine",
-    },
-    {
-      id: "1",
-      image: "/product/p5.webp",
-      name: "Digital BP Machine",
-    },
-    {
-      id: "1",
-      image: "/product/p6.webp",
-      name: "Digital BP Machine",
-    },
-    {
-      id: "1",
-      image: "/product/p7.webp",
-      name: "Digital BP Machine",
-    },
-    {
-      id: "1",
-      image: "/product/p7.webp",
-      name: "Digital BP Machine",
-    },
-    {
-      id: "1",
-      image: "/product/p8.webp",
-      name: "Digital BP Machine",
-    },
-    {
-      id: "1",
-      image: "/product/p9.webp",
-      name: "Digital BP Machine",
-    },
-    {
-      id: "1",
-      image: "/product/p10.webp",
-      name: "Digital BP Machine",
-    },
-  ];
+
   const { data, isValidating } = useSwr('product');
-  console.log(data)
   return (
     <main className=" flex flex-col justify-between items-center gap-10 w-full">
       <div className="h-fit w-full grid lg:grid-cols-4 md:grid-cols-2 grid-cols-2 gap-3">
-        {data?.data?.data?.map((item: any, index: number) => {
-          return <ProductCard key={item._id} item={item} />;
-        })}
+        {
+          isValidating ? (
+            <>
+              <Skelton />
+              <Skelton />
+              <Skelton />
+              <Skelton />
+            </>
+          ) : (
+            data?.data?.data?.map((item: any, index: number) => {
+              return <ProductCard key={item._id} item={item} />;
+            }))}
       </div>
     </main>
   );
