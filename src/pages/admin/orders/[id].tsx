58158ee434838c8fd11992de4652d6d7ad1387d1
Rajesh6290 <rajeshkumarbehera039@gmail.com>
2024-04-15 15:18:48 +0530

    changes

---
+++ src/pages/admin/orders/[id].tsx
@@ -0,0 +1,204 @@
+import Button from '@/components/core/Button'
+import CustomInputField from '@/components/core/CustomInputField'
+import useAuth from '@/hooks/useAuth'
+import useSwr from '@/hooks/useSwr'
+import { AdminLayout } from '@/layouts'
+import moment from 'moment'
+import { useRouter } from 'next/router'
+
+const OrderById = () => {
+    const { user } = useAuth()
+    const router = useRouter()
+    const { data, isValidating } = useSwr(`order/${router?.query?.id}`)
+    const item = data?.data?.data
+    console.log(data)
+    return (
+        <AdminLayout title="Product List | Terracotta">
+
+            <div className=' w-full p-5 flex flex-col gap-5'>
+                <p className="font-semibold tracking-wider text-2xl text-gray-900">Order Details</p>
+
+                <div className='w-full h-fit bg-white shadow-[rgba(17,_17,_26,_0.1)_0px_0px_16px] p-6 rounded-lg flex flex-col gap-5'>
+                    <article className='flex items-center justify-between gap-6'>
+                        <p className='flex items-center gap-1 text-sm font-medium text-gray-500'>
+                            <span>Order ID :</span>
+                            <span className=' text-gray-800'>{item?.orderNo}</span>
+
+                        </p>
+                        <p className='flex items-center gap-1 text-sm font-medium text-gray-500'>
+                            <span>Placed On :</span>
+                            <span className=' text-gray-800'>{moment(item?.createdAt).format("lll")}</span>
+
+                        </p>
+                        <Button
+
+                            type="submit"
+                        >
+                            Update
+                        </Button>
+                    </article>
+                    <div className='w-full flex items-center gap-6 border-b border-dashed py-2'>
+                        <div className='w-full flex items-center gap-3'>
+                            <img src={item?.user?.image} className='w-16 h-16 object-fill rounded-full' alt="" />
+                            <p className='flex flex-col'>
+                                <span className=' text-sm font-medium text-gray-700'>{item?.user?.name}</span>
+                                <span className=' text-sm font-medium text-gray-700'>{item?.user?.email}</span>
+                                <span className=' text-sm font-medium text-gray-700'>{item?.user?.mobileNo}</span>
+                            </p>
+                        </div>
+                        <div className='w-full'>
+                            <CustomInputField
+                                key="1"
+                                name="OrderStatus"
+                                type="select"
+                                value={item?.orderStatus ? item?.orderStatus : ""}
+                                options={[
+                                    {
+                                        label: "INITIATE",
+                                        value: "INITIATE"
+                                    },
+                                    {
+                                        label: "PICKED",
+                                        value: "PICKED"
+                                    },
+                                    {
+                                        label: "TRANSITS",
+                                        value: "TRANSITS"
+                                    },
+                                    {
+                                        label: "PROCESSING",
+                                        value: "PROCESSING"
+                                    },
+                                    {
+                                        label: "COMPLETED",
+                                        value: "COMPLETED"
+                                    },
+                                ]}
+                                onChange={(e: any) => {
+
+                                }}
+                                fullWidth
+
+                                label="Order Status"
+                            />
+                        </div>
+                    </div>
+
+                    <div className='flex flex-col gap-8 w-full'>
+                        {
+                            item?.product?.map((pre: any) => (
+                                <div key={pre?.id} className='w-full flex items-center '>
+                                    <div className='w-full flex items-center gap-4'>
+                                        <img src={pre?.image} className=' w-16 h-16 object-fill' alt="" />
+                                        <div className=' flex flex-col gap-1'>
+                                            <p className='text-lg font-semibold '>{pre?.name}</p>
+                                            <p className='flex items-center gap-1'>
+                                                <span className=' text-sm font-medium text-gray-500'>₹{pre?.discountPrice}</span>
+                                                <span className=' text-sm font-medium text-gray-500'>*</span>
+                                                <span className=' text-sm font-medium text-gray-500'>{pre?.quantity}</span>
+                                                <span className=' text-sm font-medium text-gray-500'>=</span>
+                                                <span className=' text-sm font-medium text-gray-500'>{pre?.totalSalePrice}</span>
+                                            </p>
+                                        </div>
+                                    </div>
+                                    <div className='w-full flex flex-col'>
+                                        <p className=' font-medium text-gray-600'>Product Details :</p>
+                                        <p className=' text-sm font-medium text-gray-500 capitalize'>{pre?.description}, {pre?.category}, {pre?.color}</p>
+                                    </div>
+                                </div>
+                            ))
+                        }
+
+
+                    </div>
+
+
+                </div>
+                <div className='w-full flex gap-6 h-fit'>
+                    <div className='w-full bg-white p-5 rounded-lg shadow-[rgba(17,_17,_26,_0.1)_0px_0px_16px] flex flex-col gap-5 h-full'>
+                        <div className='w-full'>
+                            <CustomInputField
+                                key="1"
+                                name="orderCancel"
+                                type="select"
+                                value={item?.isCancelled}
+                                options={[
+                                    {
+                                        label: "Order Cancelled",
+                                        value: true
+                                    },
+                                    {
+                                        label: "Not Cancelled",
+                                        value: false
+                                    }
+                                ]}
+                                onChange={(e: any) => {
+
+                                }}
+                                fullWidth
+
+                                label="Order Cancelled"
+                            />
+                        </div>
+                        <div className='w-full'>
+                            <CustomInputField
+                                key="1"
+                                name="address"
+                                type="textarea"
+                                value={item?.addresses?.landmark + item?.addresses?.address + item?.addresses?.city + item?.addresses?.state + item?.addresses?.countryCode + item?.addresses?.pincode}
+                                multiline={true}
+                                rows={4}
+                                onChange={(e: any) => {
+
+                                }}
+                                fullWidth
+
+                                label="Shipping Address"
+                            />
+                        </div>
+
+                    </div>
+                    <div className='w-full bg-white px-5 py-4 rounded-lg shadow-[rgba(17,_17,_26,_0.1)_0px_0px_16px] flex flex-col gap-3 h-full'>
+                        <p className=' font-semibold '>Total Order Summery</p>
+                        <p className=' w-full flex justify-between'>
+                            <span className='text-sm text-gray-500'>Subtotal :</span>
+                            <span className='text-sm text-gray-800'>₹{item?.amount?.totalAmount}</span>
+                        </p>
+                        <p className=' w-full flex justify-between'>
+                            <span className='text-sm text-gray-500'>Quantity :</span>
+                            <span className='text-sm text-gray-800'>{item?.amount?.totalQuantity}</span>
+                        </p>
+                        <p className=' w-full flex justify-between'>
+                            <span className='text-sm text-gray-500'>Shipping Fee :</span>
+                            <span className='text-sm text-gray-800'>₹{item?.amount?.deliveryCharge}</span>
+                        </p>
+                        <p className=' w-full flex justify-between'>
+                            <span className='text-sm text-gray-500'>Discount(%) :</span>
+                            <span className='text-sm text-gray-800'>₹{item?.amount?.discount}</span>
+                        </p>
+                        <p className=' w-full flex justify-between'>
+                            <span className='text-sm text-gray-500'>Discount Price :</span>
+                            <span className='text-sm text-gray-800'>₹{item?.amount?.discountAmount}</span>
+                        </p>
+                        <hr className=' w-full border border-dashed' />
+                        <p className=' w-full flex justify-between'>
+                            <span className='text-sm text-gray-500 '>Payment Method :</span>
+                            <span className='text-sm text-gray-900 font-medium '>COD</span>
+                        </p>
+                        <hr className=' w-full border border-dashed' />
+                        <p className=' w-full flex justify-between'>
+                            <span className='text-sm text-gray-900 font-semibold'>Total</span>
+                            <span className='text-sm text-gray-900 font-semibold'>₹{item?.amount?.totalSaleAmount}</span>
+                        </p>
+
+                    </div>
+                </div>
+
+
+            </div>
+        </AdminLayout>
+    )
+}
+
+export default OrderById
+
--- src/pages/admin/orders/index.tsx
+++ src/pages/admin/orders/index.tsx
@@ -1,25 +1,17 @@
 import CustomLoader from '@/components/core/CustomLoader'
-import EditProduct from '@/components/form/EditProductForm'
-import EditProductImage from '@/components/form/EditProductImage'
-import useMutation from '@/hooks/useMutation'
 import useSwr from '@/hooks/useSwr'
 import { AdminLayout } from '@/layouts'
 import { MuiTblOptions } from '@/utils'
 import MaterialTable from '@material-table/core'
-import { Pagination, Paper, Switch, Tooltip } from '@mui/material'
+import { Pagination, Paper } from '@mui/material'
 import moment from 'moment'
 import Link from 'next/link'
 import { useDeferredValue, useState } from 'react'
 import { BsEye } from 'react-icons/bs'
 import { IoMdSearch } from 'react-icons/io'
-import { MdDelete, MdEdit } from 'react-icons/md'
+import { MdDelete } from 'react-icons/md'
 
 const AllOrders = () => {
-    const { mutation, isLoading } = useMutation()
-    const [view, setView] = useState("Table")
-    const [open, setOpen] = useState(false);
-    const [value, setValue] = useState<any>();
-    const [imageOpen, setImageOpen] = useState(false);
     const [pageNumber, setPageNumber] = useState<number>(1);
     const [searchText, setSearchText] = useState<string>("")
     const query = useDeferredValue(searchText)
@@ -28,36 +20,10 @@ const AllOrders = () => {
 
 
     const { data, isValidating, mutate, pagination } = useSwr(url, { revalidateOnFocus: true });
-    const UpdateProduct = async (item: any) => {
-        try {
-            const res = await mutation(`product/${item?.id}`, {
-                method: "PUT",
-                body: {
-                    isPublished: item?.isPublished === true ? false : true
-                }
-            })
-            if (res?.status === 200) {
-                mutate()
-            }
-        } catch (error) {
-            console.log(error);
-        }
-    }
+
     return (
         <AdminLayout title="Product List | Terracotta">
-            <EditProduct
-                openDialog={open}
-                setOpenDialog={() => setOpen(false)}
-                mutate={mutate}
-                data={value}
 
-            />
-            <EditProductImage
-                openDialog={imageOpen}
-                setOpenDialog={() => setImageOpen(false)}
-                data={value}
-                mutate={mutate}
-            />
             <div className=' w-full p-5 flex flex-col gap-5'>
                 <p className="font-semibold tracking-wider text-xl text-gray-700">All Orders List</p>
                 <div className="w-full flex items-center  justify-between">
@@ -75,7 +41,7 @@ const AllOrders = () => {
 
                 <MaterialTable
 
-                    isLoading={isLoading || isValidating}
+                    isLoading={isValidating}
                     components={{
                         Container: (props: any) => (
                             <Paper {...props} className="!shadow-none" />
@@ -109,7 +75,6 @@ const AllOrders = () => {
                                 orderId: item?.orderNo,
                                 quantity: item?.amount?.totalQuantity,
                                 createdAt: moment(item?.createdAt).format("lll"),
-
                                 address: item?.addresses?.landmark,
                                 amount: item?.amount?.totalAmount,
                                 status: item?.orderStatus
@@ -151,9 +116,6 @@ const AllOrders = () => {
                             field: "quantity",
                             editable: "never",
                             width: "2%",
-                            render: (item: any) => (
-                                <p className=" font-medium capitalize ">{item?.category}</p>
-                            )
                         },
                         {
                             title: "Purchase Date",
@@ -196,10 +158,7 @@ const AllOrders = () => {
                             render: (item: any) => (
                                 <div className='flex items-center gap-3'>
 
-                                    <p onClick={() => {
-                                        setImageOpen(true);
-                                        setValue(item)
-                                    }}><BsEye className='text-xl text-gray-500  cursor-pointer' /></p>
+                                    <Link href={`/admin/orders/${item?.id}`}><BsEye className='text-xl text-gray-500  cursor-pointer' /></Link>
                                     <p><MdDelete className='text-xl text-red-500  cursor-pointer' /></p>
                                 </div>
                             )
